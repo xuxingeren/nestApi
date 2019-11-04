6f64f89ab7b07c6581927dc17541367839ba4fa6
徐新 <908642754@qq.com>
2019-11-04 16:48:57 +0800

    设置请求

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Response, HttpStatus, HttpCode, Post, Body } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
-  @Get()
-  finally(): string {
-    return 'cats';
+  @Get('get')
+  getId(@Response() res, @Query() query) {
+    if (query.id) {
+      res.status(HttpStatus.OK).json({
+        id: query.id,
+        code: 200,
+    });
+    } else {
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        msg: 'id没有传过来',
+        code: 500,
+    });
+    }
+  }
+  @Post('set')
+  setData(@Response() res, @Body() body) {
+    res.status(HttpStatus.OK).json({
+      name: body.name,
+      age: body.age,
+      code: 200,
+  });
   }
 }
