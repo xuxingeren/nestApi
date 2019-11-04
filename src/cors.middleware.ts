2eb4f67450a4869f1f946a29bb74c43579a58d2d
徐新 <908642754@qq.com>
2019-11-04 19:04:41 +0800

    更新中间件

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -4,49 +4,22 @@ import { Request, Response } from 'express';
 @Injectable()
 export class Cors implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
-    return (...args: any[]) => {
-      const origin = req.get('Origin');
-      // 判断是不是来自跨域的请求
-      if (origin !== undefined) {
+    const origin = req.get('Origin');
+    // 判断是不是来自跨域的请求
+    if (origin !== undefined) {
+      res.set({
+        'Access-Control-Allow-Origin': origin,
+      });
+      // 判断是不是预检请求
+      if (req.method === 'OPTIONS') {
         res.set({
-          'Access-Control-Allow-Origin': origin,
+          'Access-Control-Allow-Methods': 'GET, HEAD, PUT, PATCH, POST, DELETE',
+          'Access-Control-Allow-Headers': 'Content-Type',
         });
-        // 判断是不是预检请求
-        if (req.method === 'OPTIONS') {
-          res.set({
-            'Access-Control-Allow-Methods': 'GET, HEAD, PUT, PATCH, POST, DELETE',
-            'Access-Control-Allow-Headers': 'Content-Type',
-          });
-          res.status(204).end();
-          return;
-        }
+        res.status(204).end();
+        return;
       }
-      next();
-    };
+    }
+    next();
   }
 }
-
-// @Middleware()
-// export class Cors implements NestMiddleware {
-//   resolve(): (req, res, next) => void {
-//     return (req, res, next) => {
-//       const origin = req.get('Origin');
-//       // 判断是不是来自跨域的请求
-//       if (origin !== undefined) {
-//         res.set({
-//           'Access-Control-Allow-Origin': origin,
-//         });
-//         // 判断是不是预检请求
-//         if (req.method === 'OPTIONS') {
-//           res.set({
-//             'Access-Control-Allow-Methods': 'GET, HEAD, PUT, PATCH, POST, DELETE',
-//             'Access-Control-Allow-Headers': 'Content-Type',
-//           });
-//           res.status(204).end();
-//           return;
-//         }
-//       }
-//       next();
-//     };
-//   }
-// }
